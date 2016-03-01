9a9e55f94916899110bdbb8a5c613be64e201f8d
David Keathley <keathleydavidj@gmail.com>
2016-03-01 09:04:57 -0600

    Finally have both sessions and user auth working

---
+++ models/character.js
@@ -0,0 +1,77 @@
+var mongoose = require('mongoose');
+
+var stat = {score: Number, mod: Number};
+var skill = {
+    score: Number,
+    prof: {type: Boolean, default: false}
+};
+
+var characterSchema = new mongoose.Schema({
+  name: String,
+  player: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+  class: String,
+  race: String,
+  alignment: String,
+  xp: {type: Number, default: 0},
+  stats: {
+    str: stat,
+    dex: stat,
+    const: stat,
+    int: stat,
+    wis: stat,
+    char: stat
+  },
+  inspire: Number,
+  profBonus: Number,
+  saving: {
+    str: skill,
+    dex: skill,
+    const: skill,
+    int: skill,
+    wis: skill,
+    char: skill
+  }
+  skills: {
+    acrobatics: skill,
+    animal: skill,
+    arcana: skill,
+    athletics: skill,
+    deception: skill,
+    history: skill,
+    insight: skill,
+    intimidation: skill,
+    investigation: skill,
+    medicine skill,
+    nature: skill,
+    perception: skill,
+    performance: skill,
+    persuasion: skill,
+    religion: skill,
+    slight: skill,
+    stealth: skill,
+    survival: skill
+  },
+  armorClass: Number,
+  initative: Number,
+  speed: Number,
+  hp: {
+    max: Number,
+    current: Number,
+    Temporary: Number
+  },
+  hitDie: {
+    die: String,
+    avail: Number
+  },
+  dSaves: {
+    success: {type: Number, default: 0},
+    fail: {type: Number, default: 0}
+  },
+  created_at: Date,
+  updated_at: Date
+});
+
+var Character = mongoose.model('Character', characterSchema);
+
+// Make this available to our other files
+module.exports = Character;
---
+++ routes/passport.js
@@ -0,0 +1,54 @@
+module.exports = function(app, passport) {
+
+  // HOME PAGE (with login links) ========
+  app.get('/', function(req, res) {
+    res.render('index'); // load the index.ejs file
+  });
+
+
+  app.get('/dashboard', isLoggedIn, function(req, res, next) {
+    res.render('dashboard', {
+      'user': req.user
+    });
+  });
+
+  // SIGNIN ==============================
+  app.get('/login', function(req, res) {
+
+    // render the page and pass in any flash data if it exists
+    res.render('login', {
+      message: req.flash('signupMessage')
+    });
+  });
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/dashboard', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
+
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/dashboard', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
+
+  // LOGOUT ==============================
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated())
+    return next();
+
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+}
